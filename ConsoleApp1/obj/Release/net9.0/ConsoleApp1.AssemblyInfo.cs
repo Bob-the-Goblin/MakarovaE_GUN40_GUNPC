179d1503dd336a1ce8587c5ae8be722bd2fa7646
Bob <lizokdoughnut03@gmail.ru>
2025-11-05 18:45:37 +0300

    Second

--- ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8c89ff9a8ad31f6986929e29ce518df6482368")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+class Program
+    {
+    static void Main()
+    {
+        //First    
+        int[] fib = new int[8];
+        fib.SetValue(0, 0);
+        fib.SetValue(1, 1);
+        for (int i = 0; i < fib.Length; i++)
+        {
+            if (i > 1)
+            {
+                var a = fib[i - 2];
+                var b = fib[i - 1];
+                fib.SetValue(a + b, i);
+            }
+        }
+
+        //Second
+        string[] month = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", };
+
+        //Third
+        double[,] matrix = new double[3, 3];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = Math.Pow(j + 2, i + 1);
+            }
+        }
+
+        //Fourth
+        double[][] jag = new double[3][];
+        jag[0] = new double[5] { 1, 2, 3, 4, 5 };
+        jag[1] = new double[2] { Math.E, Math.PI };
+        jag[2] = new double[4] { Math.Log(1), Math.Log(10), Math.Log(100), Math.Log(1000) };
+
+
+        //Fifth
+        int[] array = { 1, 2, 3, 4, 5 }; int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
+
+        for (int i = 0; i < 3; i++)
+        {
+            array2[i] = array[i];
+
+        }
+
+        //Sixth
+        Array.Resize(ref array, array.Length * 2);
+        Console.WriteLine(array.Length);
+    }
+    }
+
